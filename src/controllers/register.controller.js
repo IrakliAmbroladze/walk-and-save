f39885d18b5c773f2b847a3c5df421a039d3b92e
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-21 09:46:43 +0400

    feat(register): check if email already exists in db

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -12,6 +12,16 @@ export const register = async (req, res) => {
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "passwords do not match" });
     }
+    const { data, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email);
+    if (error) {
+      throw new Error();
+    }
+    if (data.length > 0) {
+      return res.status(400).json({ message: "email already exists" });
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
