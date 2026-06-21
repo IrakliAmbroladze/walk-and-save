4b888c8f66aab6641054c5a1e36010e1d845a332
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-21 10:04:26 +0400

    feat: insert hashed password in db

--- src/controllers/register.controller.js
+++ src/controllers/register.controller.js
@@ -12,16 +12,44 @@ export const register = async (req, res) => {
     if (password !== confirmPassword) {
       return res.status(400).json({ message: "passwords do not match" });
     }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Invalid email format",
+      });
+    }
+
     const { data, error } = await supabase
       .from("users")
       .select("*")
-      .eq("email", email);
+      .eq("email", email)
+      .maybeSingle();
     if (error) {
-      throw new Error();
+      throw error;
     }
-    if (data.length > 0) {
-      return res.status(400).json({ message: "email already exists" });
+    if (data) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const { error: hashError } = await supabase.from("users").insert({
+      first_name: firstName,
+      last_name: lastName,
+      email,
+      password_hash: hashedPassword,
+    });
+
+    if (hashError) {
+      throw hashError;
+    }
+
+    return res.status(201).json({
+      message: "User registered successfully",
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: "Internal server error" });
