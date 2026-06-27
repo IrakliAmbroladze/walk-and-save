867cf142deb7c18adc91f60e939ae600cf6cf655
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-06-28 01:08:26 +0400

    feat(database): chanage database to supabase

--- server.js
+++ server.js
@@ -1,87 +1,99 @@
 import "dotenv/config";
 import app from "./app.js";
-import database from "./src/config/supabase.js";
-
+import { supabase } from "./src/config/supabase.js";
 
 app.get("/api/categories", async (req, res) => {
-const {data, error} = await database.from("categories").select("*");
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { data, error } = await supabase.from("categories").select("*");
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
 
 app.get("/api/offers", async (req, res) => {
-const {data, error} = await database.from("offers").select("*");
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { data, error } = await supabase.from("offers").select("*");
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
 
 app.get("/api/offers/:id", async (req, res) => {
-const { id } = req.params;
-const {data, error} = await database.from("offers").select("*").eq("id", id).single();
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("offers")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
 
 app.get("/api/cafe-details", async (req, res) => {
-const {data, error} = await database.from("cafe_details").select("*");
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { data, error } = await supabase.from("cafe_details").select("*");
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
 
 app.get("/api/cafe-details/:id", async (req, res) => {
-const { id } = req.params;
-const {data, error} = await database.from("cafe_details").select("*").eq("id", id).single();
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("cafe_details")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
-
 
 app.get("/api/user-info", async (req, res) => {
-const {data, error} = await database.from("user_info").select("*");
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
-})
-
-app.get("/api/user-info/:id", async (req, res) => {
-const { id } = req.params;
-const {data, error} = await database.from("user_info").select("*").eq("id", id).single();
-if (error) {
-  res.status(500).json({ error: error.message });
-}
-res.json(data);
+  const { data, error } = await supabase.from("user_info").select("*");
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
 });
 
+app.get("/api/user-info/:id", async (req, res) => {
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("user_info")
+    .select("*")
+    .eq("id", id)
+    .single();
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
+});
 
 app.get("/api/my-wallet", async (req, res) => {
-  const{data,error} =await database.from("my_wallet").select("*");
-  if(error){
-    res.status(500).json({error:error.message});
+  const { data, error } = await supabase.from("my_wallet").select("*");
+  if (error) {
+    res.status(500).json({ error: error.message });
   }
   res.json(data);
 });
 
 app.get("/api/my-wallet/:id", async (req, res) => {
-  const {id} = req.params;
-  const{data,error} =await database.from("my_wallet").select("*").eq("id",id).single(); 
-  
-if(error){
-  res.status(500).json({error:error.message});
-}   
-res.json(data);
-});
+  const { id } = req.params;
+  const { data, error } = await supabase
+    .from("my_wallet")
+    .select("*")
+    .eq("id", id)
+    .single();
 
+  if (error) {
+    res.status(500).json({ error: error.message });
+  }
+  res.json(data);
+});
 
 const PORT = process.env.PORT || 3000;
 
